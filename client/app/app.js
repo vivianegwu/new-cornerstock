3da30c74e9c7d068b3224e3925436c299237764e
Tumo505 <cse17-009@thuto.bac.ac.bw>
2020-08-03 19:30:18 +0200

    client/app/app.js to continue

--- client/app/app.js
+++ client/app/app.js
@@ -15,14 +15,14 @@ import ScrollToTop from './scrollToTop';
 import setToken from './utils/token';
 import './styles/style.scss';
 
-// Import Font Awesome Icons Set
-import 'font-awesome/css/font-awesome.min.css';
+// // Import Font Awesome Icons Set
+// import 'font-awesome/css/font-awesome.min.css';
 
-// Import Simple Line Icons Set
-import 'simple-line-icons/css/simple-line-icons.css';
+// // Import Simple Line Icons Set
+// import 'simple-line-icons/css/simple-line-icons.css';
 
-// react-bootstrap-table2 styles
-import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+// // react-bootstrap-table2 styles
+// import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
 // Authentication
 const token = localStorage.getItem('token');
