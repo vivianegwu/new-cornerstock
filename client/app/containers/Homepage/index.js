7a647fcdaa60e90c2c3e0e3f0885ebc01b4b5087
Tumo505 <cse17-009@thuto.bac.ac.bw>
2020-08-07 03:34:31 +0200

    address

--- client/app/containers/Homepage/index.js
+++ client/app/containers/Homepage/index.js
@@ -10,12 +10,13 @@ import { connect } from 'react-redux';
 import actions from '../../actions';
 import BrandList from '../../components/BrandList';
 import { MDBCol, MDBInput } from "mdbreact";
+import hcbgImage from "../../../public/images/shop-owner.jpg";
 
 
 class Homepage extends React.PureComponent {
   componentDidMount() {
    this.props.fetchBrands();
-  }
+}
 
   render() {
     const { brands } = this.props;
@@ -29,6 +30,15 @@ class Homepage extends React.PureComponent {
          containerClass="active-pink active-pink-2 mt-0 mb-3" 
          />
       </MDBCol>
+      <div
+        class="bg_image"
+        style={{
+          backgroundImage: 'url('+hcbgImage+')',
+          backgroundSize: "cover",
+          height: "45vh",
+          color: "#f5f5f5"
+        }}
+      />
         <BrandList brands={brands} />
       </div>
 
